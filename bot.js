8c7aae97e3979b382239a229d96abc0f0611e25d
logicdagod <49599889+logicdagod@users.noreply.github.com>
2019-04-13 22:36:31 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,10 +15,13 @@ client.on('message', message => {
     if (message.content === 'ping') {
 
         message.reply('pong');
-  
+
+      }
+
 });
 
 
 // THIS MUST BE THIS WAY
 
 client.login(process.env.BOT_TOKEN);
+{
